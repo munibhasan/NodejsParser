befa22186a29f740139b47e462dfafacf9660c71
Shameel Uddin <shameel.uddin@eurosofttech.co.uk>
2023-08-18 19:48:03 +0500

    logPraser capped.

--- Modules/LogsParser/model/logsparser.model.js
+++ Modules/LogsParser/model/logsparser.model.js
@@ -2,41 +2,36 @@ const mongoose = require("../../../utils/mongoose.service").mongoose;
 
 const Schema = mongoose.Schema;
 
-const logsParserSchema = new Schema({
-  IMEI: String,
-  clientId: String,
-  deviceId: String,
-  vehicleId: String,
-  client: Schema.Types.Mixed,
-  device: Schema.Types.Mixed,
-  vehicle: Schema.Types.Mixed,
-  deviceAssign: Schema.Types.Mixed,
-  payloadSocket: Schema.Types.Mixed,
-  response: new Schema({
-    occursBeforeIMEIAcknowledgement: {
-      type: Boolean,
-      default: false,
+const logsParserSchema = new Schema(
+  {
+    IMEI: String,
+    clientId: String,
+    deviceId: String,
+    vehicleId: String,
+    client: Schema.Types.Mixed,
+    device: Schema.Types.Mixed,
+    vehicle: Schema.Types.Mixed,
+    deviceAssign: Schema.Types.Mixed,
+    payloadSocket: Schema.Types.Mixed,
+    response: new Schema({
+      occursBeforeIMEIAcknowledgement: {
+        type: Boolean,
+        default: false,
+      },
+      type: String,
+      status: String,
+      message: String,
+      data: Schema.Types.Mixed,
+    }),
+  },
+  {
+    capped: {
+      size: 300 * 1024 * 1024, // 300MB in bytes
+      max: 1000000, // Maximum number of records
+      autoIndexId: true,
     },
-    type: String,
-    status: String,
-    message: String,
-    data: Schema.Types.Mixed,
-  }),
-});
-
-logsParserSchema.virtual("id").get(function () {
-  return this._id.toHexString();
-});
-
-// Ensure virtual fields are serialised.
-logsParserSchema.set("toJSON", {
-  virtuals: true,
-});
-
-logsParserSchema.findById = function (cb) {
-  return this.model("LogsParser").find({ id: this.id }, cb);
-};
-
-const LogsParser = mongoose.model("LogsParser", logsParserSchema);
+    timestamps: true,
+  }
+);
 
 exports.model = mongoose.model("LogsParser", logsParserSchema);
