f55346a95cd6877aafc9f41c06a2cde668b21c99
Shameel Uddin <shameel.uddin@eurosofttech.co.uk>
2023-08-18 18:39:19 +0500

    Redis log changed.

--- utils/redisConnectionHelper.js
+++ utils/redisConnectionHelper.js
@@ -4,7 +4,7 @@ module.exports = async function redisConnectionHelper() {
   const redisClient = redis.createClient({
     url: process.env.REDIS_SERVER_URL,
   });
-  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+  redisClient.on("error", (error) => console.log(`Redis Error : ${error}`));
   await redisClient.connect();
   return redisClient;
 };
