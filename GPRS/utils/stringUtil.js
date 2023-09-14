bed6d16878db4e5660bd3175f19c4ace49aa7e9e
Shameel Uddin <shameel.uddin@eurosofttech.co.uk>
2023-09-14 10:57:59 +0500

    stringUtil in GPRS | throw changed to console.log and return added for that purpose.

--- GPRS/utils/stringUtil.js
+++ GPRS/utils/stringUtil.js
@@ -19,9 +19,14 @@ function hexToAscII(str) {
 }
 exports.hexToAscII = hexToAscII;
 function splitAt(str, ...extraIndexes) {
-  if (str == null || str.length == 0)
-    throw new Error(`Invalid string to split.`);
-  if (extraIndexes.length == 0) throw new Error(`Must split at least once.`);
+  if (str == null || str.length == 0) {
+    console.log(`Invalid string to split.`);
+    return;
+  }
+  if (extraIndexes.length == 0) {
+    console.log(`Must split at least once.`);
+    return;
+  }
   let i = extraIndexes[0];
   let rest = extraIndexes.slice(1);
   if (i < 0) {
